19b9c5f1b3b107f96caf024f9dd4c56a772409f6
Evan Williams <evan.b.williams95@gmail.com>
2021-05-25 09:46:54 -0500

    completed console exercise walkthrough

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -25,12 +25,13 @@ public class ConsoleExercises {
 
 
 //        double pi = 3.14159;
-//        System.out.printf("The value of PI is: " + pi);
+//        System.out.format("The value of PI is approximately: %.2f ", pi);
 
 
         Scanner scanner = new Scanner(System.in);
 //        System.out.println("Give me a number.");
 //        int num = scanner.nextInt();
+//        System.out.printf("You entered: %d%n", num);
 //
 //        System.out.println("Give me three words");
 //        String firstWord = scanner.next();
@@ -38,19 +39,21 @@ public class ConsoleExercises {
 //        String thirdWord = scanner.next();
 //        System.out.printf("Your three words were: %n%s%n%s%n%s%n", firstWord, secondWord, thirdWord);
 
-//        System.out.println("Enter a sentence");
-//        String userInput = scanner.nextLine();
 //
-//        System.out.printf("Your sentence was: %s%n", userInput);
-
-        System.out.println("Give me the length and width of the classroom");
+//        System.out.println("Enter a sentence");
+//        scanner.nextLine();
 //        String userInput = scanner.nextLine();
-        Object L = scanner.nextInt();
-        Object W = scanner.nextInt();
-        int leng = (int) L;
-        int wid = (int) W;
+//        System.out.printf("Your sentence was: \"%s\"%n", userInput);
+//
 
-        System.out.printf("The area is %d while the perimeter is %d", leng*wid, (2*leng) + (2*wid));
+
+        System.out.println("Give me a length in meters:");
+        int length = Integer.parseInt(scanner.nextLine());
+        System.out.printf("Length entered: %d meters.%nGive me a width in meters:%n", length);
+        int width = Integer.parseInt(scanner.nextLine());
+        int area = length * width;
+        int perimeter = (2 * width) + (2 * length);
+        System.out.printf("Length entered: %d meters.%nWidth entered: %d meters.%nArea of room: %d meters squared.%nPerimeter of room %d meters.%n", length, width, area, perimeter);
 
 
 
