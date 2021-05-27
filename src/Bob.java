9068712d4457ce7e77a76ec76a23cf3e6e31623b
Evan Williams <evan.b.williams95@gmail.com>
2021-05-27 09:33:49 -0500

    bob walkthrough complete

--- src/Bob.java
+++ src/Bob.java
@@ -4,23 +4,27 @@ public class Bob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Talk to Bob");
-//
-//        String userResponse = "";
-//        do {
-            System.out.println("Say something to Bob.");
-            String saidSomething = scanner.nextLine();
-            if (saidSomething.endsWith("?")) {
+
+        String response = "";
+
+        while (!response.equalsIgnoreCase("QUIT")) {
+            System.out.println("What do you want?");
+            response = scanner.nextLine();
+            if (response.endsWith("?")) {
                 System.out.println("Sure");
-            }else if (saidSomething.endsWith("!")) {
+            }else if (response.endsWith("!")) {
                 System.out.println("Whoa, chill out!");
-            }else if (saidSomething.isEmpty()) {
+            }else if (response.isEmpty()) {
                 System.out.println("Fine. Be that way!");
             }else {
                 System.out.println("Whatever");
             }
+        }
+        System.out.println("I'm out of here...");
 
-//            System.out.println("Would like to continue?\nEnter yes to continue or no to stop.");
-//            userResponse = scanner.next();
-//        } while (userResponse.equals("yes"));
+
+
+
+
     }
 }
