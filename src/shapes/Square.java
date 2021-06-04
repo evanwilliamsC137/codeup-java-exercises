53d26bf27b0e013fcffa18f821dfe403afb8690f
Evan Williams <evan.b.williams95@gmail.com>
2021-06-04 14:13:37 -0500

    added walkthrough on rectangle and square

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,15 +1,49 @@
+
 package shapes;
 
 public class Square extends Rectangle{
-    public Square(double side) {
+
+    //this is "the side variable"
+    private double side;
+
+    //pass the variable of "side" to the parent class constructor.
+    public Square(double side){
         super(side, side);
+        //set "the side variable" equal to whats being passed into the Constructor
+        this.side = side;
     }
+    //by calling super() we can access the original public Rectangle constructor/method we created
+    //However this method requires two arguments: length and width so we have to set those values equal to something or the code wont run.
+    //we can do this by calling "side" twice to fill the values for Length & width.
 
-    public double getArea() {
-        return length*length;
-    }
-
+    @Override
     public double getPerimeter() {
-        return 4*length;
+        System.out.println("This is overridden");
+        return 4 * side;
+    }
+
+    @Override
+    public double getArea() {
+        System.out.println("This is overridden");
+        return side * side;
     }
 }
+
+
+//Shorter way
+//package shapes;
+//
+//public class Square extends Rectangle{
+//    public Square(double side) {
+//        super(side, side);
+//    }
+//
+//
+//    public double getArea() {
+//        return length*length;
+//    }
+//
+//    public double getPerimeter() {
+//        return 4*length;
+//    }
+//}
